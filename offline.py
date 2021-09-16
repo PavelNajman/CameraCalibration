99a3248fa41fd1681b5b34fd43e7b7c98852f9bf
Pavel Najman <najman.pavel@gmail.com>
2021-09-16 10:38:29 +0200

    Remove unnecessary arguments.

--- offline.py
+++ offline.py
@@ -1,30 +1,27 @@
 import cv2
 import sys
-import time
 import pathlib
 import argparse
 import numpy as np
 
 import common
 
-def parse_args():
+def ParseCommandLineArguments():
     parser = argparse.ArgumentParser(description='Detects chessboard pattern and calibrates the camera.')
-    parser.add_argument('-iw', '--image-width', default=640, type=int)
-    parser.add_argument('-ih', '--image-height', default=480, type=int)
-    parser.add_argument('-f', '--fps', default=30, type=int)
     parser.add_argument('-c', '--chessboard-columns', default=5, type=int)
     parser.add_argument('-r', '--chessboard-rows', default=4, type=int)
     parser.add_argument('-s', '--chessboard-field-size', default=0.0245, type=float)
     return parser.parse_args(sys.argv[1:])
 
-if __name__ == "__main__":
-    args = parse_args()
+def main():
+    args = ParseCommandLineArguments()
 
     OBJ_POINTS = np.zeros((1, args.chessboard_rows * args.chessboard_columns, 3), np.float32)
     OBJ_POINTS[0,:,:2] = np.mgrid[0:args.chessboard_rows, 0:args.chessboard_columns].T.reshape(-1, 2) * args.chessboard_field_size
 
     CHESSBOARD = (args.chessboard_rows, args.chessboard_columns)
 
+    shape = None
     image_points = []
     object_points = []
     for f in pathlib.Path.cwd().glob("*.jpg"):
@@ -39,9 +36,14 @@ if __name__ == "__main__":
             object_points.append(OBJ_POINTS)
             # collect image points
             image_points.append(corners)
-            # draw corners
-            image = cv2.drawChessboardCorners(image, CHESSBOARD, corners, found)
-    camera_matrix_guess = np.array([[1250.0, 0, (args.image_width-1)/2.0], [0, 1250.0, (args.image_height-1)/2.0], [0, 0, 1.0]])
+            # ensure that all processed images have the same shape
+            if not shape:
+                shape = image.shape
+            elif shape != image.shape:
+                print("ERROR: All images should have the same size")
+                return -1
+    height, width = shape
+    camera_matrix_guess = np.array([[1250.0, 0, (width-1)/2.0], [0, 1250.0, (height-1)/2.0], [0, 0, 1.0]])
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(object_points, image_points, image.shape[::-1], camera_matrix_guess, None, flags = common.CALIBRATE_CAMERA_FLAGS)
 
     print("Error: ", ret)
@@ -52,4 +54,10 @@ if __name__ == "__main__":
     print(rvecs)
     print(tvecs)
 
-    common.DumpResults(args.image_width, args.image_height, mtx, dist)
+    common.DumpResults(width, height, mtx, dist)
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
+
